0e9fef6002fb05b085821d0116257df81c26eb96
Ilyasse <ilyassesalama7@gmail.com>
2025-09-19 15:11:37 +0100

    feat: Implement add new order form

--- frontend/src/home/AddOrder.vue
+++ frontend/src/home/AddOrder.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { Card, CardHeader, CardTitle, CardContent, CardAction } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  <div>
-    <h1>Add Order</h1>
-  </div>
+  <Card class="min-w-md p-4">
+    <CardHeader class="p-0">
+      <CardTitle>Enter order details</CardTitle>
+    </CardHeader>
+
+    <CardContent class="p-0 flex flex-col gap-4">
+      <Input placeholder="Order title" />
+      <Input placeholder="Total price" />
+      <Input placeholder="Image url" />
+    </CardContent>
+
+    <CardAction class="p-0 w-full">
+      <Button class="w-full cursor-pointer" size="sm">Add Order</Button>
+    </CardAction>
+  </Card>
 </template>
